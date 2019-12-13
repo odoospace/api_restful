7e566afe77ae4470aa08b7b1e6167cbf47e4f197
Pedro Gracia <pedro.gracia@impulzia.com>
2019-12-13 10:37:56 +0100

    - add branch 12.0

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'api',
-    'version': '0.1',
+    'version': '12.0.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
